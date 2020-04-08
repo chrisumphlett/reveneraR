fd45a5a735efc8335f85809c196afe1756b04499
Chris Umphlett <christopher.umphlett@gmail.com>
2020-04-08 09:26:22 -0400

    continued refinement, committing so i can run large test on v/m

--- R/get_client_metadata.R
+++ R/get_client_metadata.R
@@ -35,8 +35,7 @@
 #' }
 
 
-product_df_base <- tibble(revulytics_product_id = numeric(), client_id = character(),
-                          property_friendly_name = character(), property_value = character())
+
 
 # for the function, user should provide a list of the property_friendly_names (custom or regular)
 
@@ -49,6 +48,9 @@ product_properties <- get_product_properties(prods, session_id, rev_user)
 desired_properties <- c("Job To Be Done", "Previous Snagit Version", "License Key", "Revulytics Machine ID")
 
 get_client_metadata <- function(rev_product_ids, rev_session_id, rev_username, desired_properties) {
+  
+  product_df_base <- tibble(revulytics_product_id = numeric(), client_id = character(),
+                            property_friendly_name = character(), property_value = character())
   
   get_one_product_metadata <- function(a) {
   
@@ -65,30 +67,51 @@ get_client_metadata <- function(rev_product_ids, rev_session_id, rev_username, d
     # print(custom_property_friendly_names)
     i <- 0
     
-    keep_going <- ifelse(!exists("content_json"), TRUE, ifelse(content_json$reachedEnd == "true", TRUE, FALSE))
+    keep_going <- TRUE
     
     while (keep_going == TRUE) {
-      print(i)
+      print(paste0("iteration ", i))
       
-      i<-i+1
+      i <- i + 1
       
-      request_body <- list(
-        user = rev_user,
-        sessionId = session_id,
-        productId = a,
-        startAtClientId = ifelse(exists("content_json"), content_json$nextClientId, NA_character_),
-        properties = array(c(custom_property_names))
-      )
+      body <- paste0("{\"user\":\"trackerbird@techsmith.com\",\"sessionId\":\"",
+                     session_id,
+                     "\",\"productId\":",
+                     a,
+                     ",\"startAtClientId\":",
+                     jsonlite::toJSON(ifelse(exists("content_json"), content_json$nextClientId, NA_character_), auto_unbox = TRUE),
+                     ",\"globalFilters\":{\"dateInstalled\":{\"type\":\"dateRange\",\"min\":\"2020-03-01\", \"max\":\"2020-03-01\"}},",
+                     paste0("\"properties\":", jsonlite::toJSON(array(c(custom_property_names)), auto_unbox = TRUE), "}"),
+                     # ",\"properties\":[\"licenseKey\",\"C01\",\"C06\",\"machineId\"]}",
+      sep = "")
+      
+      # print(cat(body))
+      
+      # request_body <- list(
+      #   user = rev_user,
+      #   sessionId = session_id,
+      #   productId = a,
+      #   startAtClientId = ifelse(exists("content_json"), content_json$nextClientId, NA_character_),
+      #   properties = array(c(custom_property_names)),
+      #   globalFilters = array(c("dateInstalled", list(list(type = "dateRange", min = "2020-03-01", max = "2020-03-02"))))
+      #   # junk = list(list(a = "b"))
+      # )
       
       # print(ifelse(exists("content_json"), content_json$nextClientId, NA_character_))
       
-      body <- jsonlite::toJSON(request_body, auto_unbox = TRUE)
+      # body <- jsonlite::toJSON(request_body, auto_unbox = TRUE)
+      # print(cat(body))
+      
+      
+      
       request <- httr::POST("https://api.revulytics.com/reporting/clientPropertyList",
                             body = body,
                             encode = "json")
+      # print(request$status)
       request_content <- httr::content(request, "text", encoding = "ISO-8859-1")
       # get_text
       content_json <- jsonlite::fromJSON(request_content, flatten = TRUE)
+      print(paste0("reachedEnd = ", content_json$reachedEnd))
       
       build_data_frame <- function(c){
         properties <- as.data.frame(content_json$results[c])
@@ -115,6 +138,9 @@ get_client_metadata <- function(rev_product_ids, rev_session_id, rev_username, d
       product_df_base <- bind_rows(product_df2, product_df_base)
       
       # print(head(product_df_base))
+      
+      keep_going <- ifelse(content_json$reachedEnd == "FALSE", TRUE, FALSE)
+      print(paste0("keep_going = ", keep_going))
       
     }
     
